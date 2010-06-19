ec7f8d8c1c6caa51cab0ee547785afc3c6d41264
Barry Schwartz <github@crudfactory.com>
2010-06-18 22:51:35 -0500

    Allow "man fonts.conf". Some work on the new names fields.

--- src/fclist.c
+++ src/fclist.c
@@ -22,6 +22,28 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+/*
+  Copyright © 2010 Barry Schwartz
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
 #include "fcint.h"
 #include <stdlib.h>
 
@@ -404,6 +426,18 @@ FcListAppend (FcListHashTable	*table,
 		familyidx = FcGetDefaultObjectLangIndex (font, FC_FAMILYLANG_OBJECT);
 	    defidx = familyidx;
 	}
+	else if (!strcmp (os->objects[o], FC_PREFERRED_FAMILY) || !strcmp (os->objects[o], FC_PREFERRED_FAMILYLANG))
+	{
+	    if (familyidx < 0)
+		familyidx = FcGetDefaultObjectLangIndex (font, FC_PREFERRED_FAMILYLANG_OBJECT);
+	    defidx = familyidx;
+	}
+	else if (!strcmp (os->objects[o], FC_WWS_FAMILY) || !strcmp (os->objects[o], FC_WWS_FAMILYLANG))
+	{
+	    if (familyidx < 0)
+		familyidx = FcGetDefaultObjectLangIndex (font, FC_WWS_FAMILYLANG_OBJECT);
+	    defidx = familyidx;
+	}
 	else if (!strcmp (os->objects[o], FC_FULLNAME) || !strcmp (os->objects[o], FC_FULLNAMELANG))
 	{
 	    if (fullnameidx < 0)
@@ -414,6 +448,18 @@ FcListAppend (FcListHashTable	*table,
 	{
 	    if (styleidx < 0)
 		styleidx = FcGetDefaultObjectLangIndex (font, FC_STYLELANG_OBJECT);
+	    defidx = styleidx;
+	}
+	else if (!strcmp (os->objects[o], FC_PREFERRED_STYLE) || !strcmp (os->objects[o], FC_PREFERRED_STYLELANG))
+	{
+	    if (styleidx < 0)
+		styleidx = FcGetDefaultObjectLangIndex (font, FC_PREFERRED_STYLELANG_OBJECT);
+	    defidx = styleidx;
+	}
+	else if (!strcmp (os->objects[o], FC_WWS_STYLE) || !strcmp (os->objects[o], FC_WWS_STYLELANG))
+	{
+	    if (styleidx < 0)
+		styleidx = FcGetDefaultObjectLangIndex (font, FC_WWS_STYLELANG_OBJECT);
 	    defidx = styleidx;
 	}
 	else
--- src/fcname.c
+++ src/fcname.c
@@ -22,6 +22,28 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
+/*
+  Copyright © 2010 Barry Schwartz
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
 #include "fcint.h"
 #include <ctype.h>
 #include <stdlib.h>
@@ -75,7 +97,15 @@ static const FcObjectType _FcBaseObjectTypes[] = {
     { FC_EMBOLDEN,	FcTypeBool },
     { FC_EMBEDDED_BITMAP,   FcTypeBool },
     { FC_DECORATIVE,	FcTypeBool },
-    { FC_LCD_FILTER,	FcTypeInteger }, /* 41 */
+    { FC_LCD_FILTER,	FcTypeInteger },
+    { FC_PREFERRED_FAMILY,	FcTypeString, },
+    { FC_PREFERRED_FAMILYLANG,	FcTypeString, },
+    { FC_PREFERRED_STYLE,		FcTypeString, },
+    { FC_PREFERRED_STYLELANG,	FcTypeString, },
+    { FC_WWS_FAMILY,	FcTypeString, },
+    { FC_WWS_FAMILYLANG,	FcTypeString, },
+    { FC_WWS_STYLE,		FcTypeString, },
+    { FC_WWS_STYLELANG,	FcTypeString, }, /* 49 */
 };
 
 #define NUM_OBJECT_TYPES    (sizeof _FcBaseObjectTypes / sizeof _FcBaseObjectTypes[0])
